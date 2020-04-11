75986ac4b5360800025874fcdfa6df3e9360ca26
notauser-01 <donaldisfirst@gmail.com>
2020-04-11 01:05:53 -0400

    My girlfriend found 3 new gray hairs today, but this is getting easier with each repository.

---
+++ MyAlarm.py
@@ -0,0 +1,33 @@
+import datetime
+import os
+import time
+
+now = str(datetime.datetime.now())[11:16] 
+alarm_time = input("When do you want to wake up (HH:MM)? ")
+
+while len(alarm_time) <= 4:
+	alarm_time = input("Do it right! (HH:MM) ")
+
+alarm_nums = alarm_time[0:2] + alarm_time[3:]
+colon = alarm_time[2]
+runclock = False
+ 
+while runclock == False:
+	if alarm_nums.isdigit() == True and colon == ":":
+			if 0 <= int(alarm_nums[0:2]) <= 23 and 0 <= int(alarm_nums[3:]) <=59:
+				runclock = True
+	else:
+		alarm_time = input("Do it right! (HH:MM) ")
+		alarm_nums = alarm_time[0:2] + alarm_time[3:]
+		colon = alarm_time[2]
+
+while now != alarm_time:
+	print(f"It is currently {now}, so you can sleep for longer.")
+	time.sleep(.2)
+	now = str(datetime.datetime.now())[11:16]
+
+file = "03 Bleed.flac"
+
+while now == alarm_time:
+	os.startfile(file)
+	time.sleep(1)
