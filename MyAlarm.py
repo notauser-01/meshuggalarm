ee3e3a2a5d73d302dd50c99d0ba54a2d1eb84c79
notauser-01 <donaldisfirst@gmail.com>
2020-04-12 00:22:13 -0400

    made it count seconds so it's more fun to watch, also removed the for loop

--- MyAlarm.py
+++ MyAlarm.py
@@ -11,11 +11,10 @@ while len(alarm_time) <= 4:
 alarm_nums = alarm_time[0:2] + alarm_time[3:]
 colon = alarm_time[2]
 runclock = False
- 
+
 while runclock == False:
-	if alarm_nums.isdigit() == True and colon == ":":
-			if 0 <= int(alarm_nums[0:2]) <= 23 and 0 <= int(alarm_nums[3:]) <=59:
-				runclock = True
+	if alarm_nums.isdigit() == True and colon == ":" and 0 <= int(alarm_nums[0:2]) <= 23 and 0 <= int(alarm_nums[2:]) <=59:
+		runclock = True
 	else:
 		alarm_time = input("Do it right! (HH:MM) ")
 		while len(alarm_time) <= 4:
@@ -24,10 +23,10 @@ while runclock == False:
 		colon = alarm_time[2]
 
 while now != alarm_time:
-	print(f"It is currently {now}, so you can sleep for longer.")
-	for i in range(240):
-		time.sleep(.25)
-		now = str(datetime.datetime.now())[11:16]
+	hrminsec = str(datetime.datetime.now())[11:19]
+	print(f"It is currently {hrminsec}, so you can sleep for longer.")
+	time.sleep(1)
+	now = str(datetime.datetime.now())[11:16]
 
 file = "03 Bleed.flac"
 os.startfile(file)
